ceb465293c82566c1b648027726ae2f5d2f3e633
RenataCehajic <renata.cehajic@gmail.com>
2022-03-02 14:06:20 +0100

    Adding styling for About and Projects

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -5,21 +5,25 @@ import classes from "./MainNavigation.module.css";
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>
+      <div>
         <a href="mailto:renata.cehajic@gmail.com">
           <strong className={classes.emailBold}>renata</strong>
           .cehajic@gmail.com
         </a>
       </div>
       <nav>
-        <ul>
-          <li>
-            <Link href="/">All Meetups</Link>
-          </li>
-          <li>
-            <Link href="/new-meetup">Add New Meetup</Link>
-          </li>
-        </ul>
+        <div className={classes.about}>
+          <ul>
+            <li>
+              <Link className={classes.a} href="/">
+                about.
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects">projects.</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
