1146bccc3698b0b09badf183b340de91de26d5b6
RenataCehajic <renata.cehajic@gmail.com>
2022-03-02 13:01:12 +0100

    Adding Header

---
+++ components/layout/MainNavigation.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import classes from "./MainNavigation.module.css";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <a href="mailto:renata.cehajic@gmail.com">
+          <strong className={classes.emailBold}>renata</strong>
+          .cehajic@gmail.com
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link href="/new-meetup">Add New Meetup</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 // our-domain.com/
 import Link from "next/link";
 import { Fragment } from "react";
+import MainNavigation from "../components/layout/MainNavigation";
 
 function HomePage() {
   return (
     <Fragment>
+      <MainNavigation />
       <ul>
         <li>
           <Link href="/projects">NextJS Is A Great Framework</Link>
