7fbc6da3fad15ecf14777542c7fb780d64378ebe
RenataCehajic <renata.cehajic@gmail.com>
2022-03-02 14:21:35 +0100

    Added profile photo

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 // our-domain.com/
 import Link from "next/link";
+import Image from "next/image";
 import { Fragment } from "react";
 import MainNavigation from "../components/layout/MainNavigation";
+import image from "../public/images/profilefoto/RenatasFB.jpg";
 
 function HomePage() {
   return (
     <Fragment>
       <MainNavigation />
+      <Image src={image} alt="RenatasFB" width="400" height="260" />
       <ul>
         <li>
           <Link href="/projects">NextJS Is A Great Framework</Link>
